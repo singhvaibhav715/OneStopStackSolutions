0d677c376f354c92d61d08419879ec6982a22c18
Vaibhav Singh <singhvaibhav715@gmail.com>
2022-04-22 10:07:43 +0530

    Added Changes

---
+++ one-stop-stack-solutions/src/app/latest-notifications-module/latest-notifications-module.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LatestNotificationsModuleRoutingModule } from './latest-notifications-module-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LatestNotificationsModuleRoutingModule
+  ]
+})
+export class LatestNotificationsModuleModule { }
