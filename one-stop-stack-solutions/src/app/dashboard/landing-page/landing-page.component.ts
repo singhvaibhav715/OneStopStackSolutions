d6d89a3ee281604b223eb0b35bbeb2825ca9b8ad
Vaibhav Singh <singhvaibhav715@gmail.com>
2022-04-23 09:17:41 +0530

    Implemented Dark Theme

--- one-stop-stack-solutions/src/app/dashboard/landing-page/landing-page.component.ts
+++ one-stop-stack-solutions/src/app/dashboard/landing-page/landing-page.component.ts
@@ -12,4 +12,31 @@ export class LandingPageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  dark:boolean=false
+  logoName="logo"
+  toggleTheme(){
+    this.dark=!this.dark
+    this.dark===true?this.changeTheme(this.darkColor):this.changeTheme(this.lightColor)
+    this.dark===true?this.logoName="logo-white":this.logoName="logo"
+  }
+
+  lightColor={
+    primaryColor:"#dde1e7",
+    textColor:"black",
+    clipBGPrimary:"rgb(2, 0, 36)",
+    clipBG:"linear-gradient(90deg,rgba(2, 0, 36, 1) 0%,rgba(9, 121, 92, 0.9808123078332895) 0%,rgba(0, 212, 255, 1) 100%)"
+  }
+  darkColor={
+    primaryColor:"#2a2a2c",
+    textColor:"white",
+    clipBGPrimary:"rgb(111,119,113)",
+    clipBG:"linear-gradient(90deg,rgba(111, 119, 113, 1) 10%,rgba(82, 93, 145, 1) 68%)"
+  }
+
+  changeTheme(color:any){
+    document.documentElement.style.setProperty('--primary-color',color.primaryColor)
+    document.documentElement.style.setProperty('--theme-text-color',color.textColor)
+    document.documentElement.style.setProperty('--theme-clip',color.clipBG)
+  }
+
 }
